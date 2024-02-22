0a63e85ae69db7933a12f2d68b9d2e5e6b9bf1e4
Delinsh <nick.delinsh@gmail.com>
2024-02-22 21:15:38 +0300

    5th step 22.02

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,32 +1,36 @@
 import random
 
-print('Answer "yes" if the number is even, otherwise answer "no".')
+def main():
+    print('Answer "yes" if the number is even, otherwise answer "no".')
 
 
-def random_even(num):
-    if num % 2 == 0:
-        question = 'yes'
-    else:
-        question = 'no'
-    return question
+    def random_even(num):
+        if num % 2 == 0:
+            question = 'yes'
+        else:
+            question = 'no'
+        return question
 
 
-game = True
-counter = 0
+    game = True
+    counter = 0
 
-while game or counter != 3:
-    number = random.randint(1, 100)
-    print(f'Question: {number}')
-    answer = input()
+    while game or counter != 3:
+        number = random.randint(1, 100)
+        print(f'Question: {number}')
+        answer = input()
 
-    if answer == random_even(number):
-        print('Correct!')
-        counter += 1
-    else:
-        print(f'{answer} is a wrong answer ;(. Correct answer was "{random_even(number)}".')
-        print(f"Let's try again!")
-        game = False
+        if answer == random_even(number):
+            print('Correct!')
+            counter += 1
+        else:
+            print(f'{answer} is a wrong answer ;(. Correct answer was "{random_even(number)}".')
+            print(f"Let's try again!")
+            game = False
 
-    if counter == 3:
-        game = False
-        print(f'Congratulations!')
+        if counter == 3:
+            game = False
+            print(f'Congratulations!')
+
+if __name__ == '__main__':
+    main()
